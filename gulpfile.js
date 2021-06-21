57365466fe9d0e690e879b5088d876a15e6bbc8d
jakeyarrington <jake@yarrington.co.uk>
2021-06-21 10:15:43 +0100

    Messing

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var notify = require("gulp-notify");
 var connect = require('gulp-connect');
+var inline = require('gulp-inline');
+var autoprefixer = require('gulp-autoprefixer');
 
 //js files
 gulp.task('scripts', function() {
@@ -16,10 +18,28 @@ gulp.task('scripts', function() {
   // pipe the js through concat, console log stripping, uglification and then store
   return gulp.src(js_src)
       .pipe(concat('app.min.js')) // concat all files in the src
-      // .pipe(striplog())
+      .pipe(striplog())
+      // .pipe(uglify())
       .pipe(gulp.dest(js_dest)) // save the file
       .on('error', gutil.log); 
 });
+
+gulp.task('inline', function() {
+  return;
+  gulp.src('src/html/index.html')
+  .pipe(inline({
+    base: 'dist/',
+    disabledTypes: ['svg', 'img', 'png'],
+    ignore: ['dist/index.html'],
+    js: function() {
+      return uglify({
+          mangle: false
+      });
+    }
+  }))
+  .pipe(gulp.dest('dist/'));
+})
+
 
 gulp.task('css', function() {  
   return gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'src/scss/*.scss']) 
@@ -45,8 +65,8 @@ gulp.task('webserver', function() {
 
 gulp.task('watch', function(){
     connect.server();
-  gulp.watch(['src/scss/*.scss'], gulp.series('css')); // Watch and run sass on changes
-  gulp.watch('src/js/*.js', gulp.series('scripts')); // Watch and run javascripts on changes
+  gulp.watch(['src/scss/*.scss'], gulp.series('css', 'inline')); // Watch and run sass on changes
+  gulp.watch('src/js/*.js', gulp.series('scripts', 'inline')); // Watch and run javascripts on changes
   gulp.src('assets/*')
     .pipe(notify('An asset has changed'));
 });
