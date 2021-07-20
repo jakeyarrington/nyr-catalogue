a14dc140f73cbe09104479d82bb11e4c815219f5
jakeyarrington <jake@yarrington.co.uk>
2021-07-20 20:58:18 +0100

    Update pwa-sw.js

--- pwa-sw.js
+++ pwa-sw.js
@@ -2,10 +2,6 @@ self.addEventListener('fetch', function(event) {
   var ext = event.request.url.slice((Math.max(0, event.request.url.lastIndexOf(".")) || Infinity) + 1);
   event.respondWith(
       caches.match(event.request).then(function(response) {
-          console.log(ext);
-          if(ext.indexOf('json') > -1) {
-            return caches.delete(event.request);
-          }
           return response || fetch(event.request);
       })
   );
