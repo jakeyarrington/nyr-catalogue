0025532ab5ba4e1500709be5b6139ad53799c4e9
jakeyarrington <jake@yarrington.co.uk>
2021-07-20 20:42:35 +0100

    Fixes

--- pwa-sw.js
+++ pwa-sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url.slice((Math.max(0, fname.lastIndexOf(".")) || Infinity) + 1));
+  console.log(event.request.url.slice((Math.max(0, event.request.url.lastIndexOf(".")) || Infinity) + 1));
   event.respondWith(
       caches.match(event.request).then(function(response) {
           return response || fetch(event.request);
