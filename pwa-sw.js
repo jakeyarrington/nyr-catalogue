4c2cb938bbdb756ea8a91be438a035ae4f790850
jakeyarrington <jake@yarrington.co.uk>
2021-07-20 21:00:25 +0100

    Update pwa-sw.js

--- pwa-sw.js
+++ pwa-sw.js
@@ -2,22 +2,10 @@ self.addEventListener('fetch', function(event) {
   var ext = event.request.url.slice((Math.max(0, event.request.url.lastIndexOf(".")) || Infinity) + 1);
   event.respondWith(
       caches.match(event.request).then(function(response) {
+          if(ext.indexOf('json') > -1) {
+            return false;
+          }
           return response || fetch(event.request);
       })
-  );
-});
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          console.log(cacheName);
-         // if (CACHE_NAME !== cacheName &&  cacheName.startsWith("gih-cache")) {
-          //  return caches.delete(cacheName);
-         // }
-        })
-      );
-    })
   );
 });
